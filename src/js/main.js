7792c10e0d51c779f88f681901a871aa172954ba
NotEternal <plainliving@protonmail.com>
2020-08-23 15:25:26 +0800

    Change style header and history. Add event and sandler on calculator button

--- src/js/main.js
+++ src/js/main.js
@@ -20,13 +20,21 @@ const calculator = {
   },
 
   getResult() {
-    // проверять на наличие запрещенных символов
+    // проверять на наличие запрещенных символов (unicode)
     // получить значение поля и разбить на массив символов
     // проверить наличие ошибок в операторах
     // разбить массив на отдельные операции по приоритету
     // выполнить каждую операцию в зависимости от приоритета
     // записывать результат каждой операции в result
     return this.result;
+  },
+
+  showError() {
+    this.error.removeAttribute("hidden");
+  },
+
+  hideError() {
+    this.error.setAttribute("hidden", "");
   },
 };
 
@@ -46,19 +54,53 @@ calculator.calc.addEventListener("click", (event) => {
         break;
 
       case "btn-result":
-        // TODO: вынести в отдельную функцию
-        const result = calculator.getResult();
-
-        if (result) {
-          const li = (document.createElement("li").innerHTML = result);
-
-          calculator.input.value = result;
-          document.querySelector(".history__list").append(li);
-        } else {
-          // view error
-        }
-        // TODO:
+        viewResult();
         break;
     }
   }
 });
+
+calculator.input.addEventListener("input", () => {
+  calculator.hideError();
+});
+
+calculator.input.addEventListener("keydown", (event) => {
+  if (event.code === "Backspace") {
+    calculator.delete();
+    // предотвращает второй вызов delete(),
+    // который происходит при всплытии
+    event.preventDefault();
+  } else if (event.code === "Enter") {
+    viewResult();
+  } else if (!checkCorrectCode(event.key)) {
+    event.preventDefault();
+  }
+});
+
+function viewResult() {
+  const result = calculator.getResult();
+
+  if (result) {
+    const li = document.createElement("li");
+    li.innerHTML = result;
+
+    calculator.input.value = result;
+    document.querySelector(".history__list").append(li);
+  } else {
+    calculator.showError();
+  }
+}
+
+function checkCorrectCode(key) {
+  return (
+    (key >= "0" && key <= "9") ||
+    key === "+" ||
+    key === "-" ||
+    key === "*" ||
+    key === "/" ||
+    key === "%" ||
+    key === "(" ||
+    key === ")" ||
+    key === "."
+  );
+}
