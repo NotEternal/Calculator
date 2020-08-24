fafe979d02ee8c2ba11e738b2319b8e1f28bbba4
NotEternal <plainliving@protonmail.com>
2020-08-24 10:03:19 +0800

    Change style for button and history. Add check for user expressions

--- src/js/main.js
+++ src/js/main.js
@@ -1,76 +1,154 @@
-"use strict";
+'use strict';
 
 const calculator = {
-  calc: document.querySelector(".calc"),
-  input: document.querySelector(".calc__input"),
-  error: document.querySelector(".calc__error"),
+  calc: document.querySelector('.calc'),
+  input: document.querySelector('.calc__input'),
+  error: document.querySelector('.calc__error'),
   result: 0,
 
   delete() {
-    if (this.input.value != "") {
-      const resStr = this.input.value.split("");
+    if (this.input.value != '') {
+      const resStr = this.input.value.split('');
 
       resStr.pop();
-      this.input.value = resStr.join("");
+      this.input.value = resStr.join('');
+      this.hideError();
     }
   },
 
   clear() {
-    this.input.value = "";
+    if (this.input.value != '') {
+      this.input.value = '';
+      this.hideError();
+    }
   },
 
   getResult() {
-    // проверять на наличие запрещенных символов (unicode)
-    // получить значение поля и разбить на массив символов
-    // проверить наличие ошибок в операторах
-    // разбить массив на отдельные операции по приоритету
-    // выполнить каждую операцию в зависимости от приоритета
+    const arrSimbols = this.input.value.split('');
+
+    checkCorrectSimbols(arrSimbols);
+
+    // TODO: разбить массив на отдельные операции по приоритету
+    // TODO: выполнить каждую операцию в зависимости от приоритета
     // записывать результат каждой операции в result
     return this.result;
   },
 
   showError() {
-    this.error.removeAttribute("hidden");
+    this.error.removeAttribute('hidden');
   },
 
   hideError() {
-    this.error.setAttribute("hidden", "");
+    this.error.setAttribute('hidden', '');
   },
 };
 
-calculator.calc.addEventListener("click", (event) => {
-  if (event.target.tagName === "BUTTON") {
+function checkCorrectSimbols(arrSimb) {
+  console.log(checkErrSimbolsInStartAndEnd(arrSimb));
+  console.log(checkTwoNoDigitSimbols(arrSimb));
+  console.log(checkBrackets(arrSimb));
+
+  if (
+    checkErrSimbolsInStartAndEnd(arrSimb) &&
+    checkTwoNoDigitSimbols(arrSimb) &&
+    checkBrackets(arrSimb)
+  ) {
+    return true;
+  }
+}
+
+// ! CHANGE NAME FUNCTION
+function checkErrSimbolsInStartAndEnd(arr) {
+  // ! ПОМЕНЯТЬ ЭТУ ДИЧЬ НА НОРМАЛЬНЫЕ СИМВОЛЫ
+  const arrStartSimbErr = ['%', '*', '/', '-', ')', 'x2'];
+  const arrEndSimbErr = ['%', '*', '/', '-', '+', 'cor', '('];
+
+  if (
+    arrStartSimbErr.includes(arr[0]) ||
+    arrEndSimbErr.includes(arr[arr.length - 1])
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
+// ! CHANGE NAME FUNCTION
+function checkTwoNoDigitSimbols(arr) {
+  // проверка на идущие подряд не цифровые символы
+  for (let i = 0; i < arr.length - 2; i += 1) {
+    if (Object.is(Number(arr[i]), NaN) && Object.is(Number(arr[i + 1]), NaN)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkBrackets(arr) {
+  const arrBrackets = [];
+
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    if (arr[i] === '(' || arr[i] === ')') {
+      arrBrackets.push(arr[i]);
+    }
+  }
+
+  // не четное кол-во скобок
+  if (arrBrackets.length % 2 != 0) {
+    return false;
+  }
+
+  const arrOpenBrakets = arrBrackets.slice(0, arrBrackets.length / 2 + 1);
+  const arrCloseBrakets = arrBrackets.slice(
+    arrBrackets.length / 2 + 1,
+    arrBrackets.length
+  );
+
+  // не правильная последовательность скобок
+  if (arrOpenBrakets.includes(')') || arrCloseBrakets.includes('(')) {
+    return false;
+  }
+
+  return true;
+}
+
+// EVENTS AND SANDLERS ------------------------------
+
+calculator.calc.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
     switch (event.target.className) {
-      case "":
+      case '':
         calculator.input.value += event.target.textContent;
         break;
 
-      case "btn-delete":
+      case 'btn-delete':
         calculator.delete();
         break;
 
-      case "btn-clear":
+      case 'btn-clear':
         calculator.clear();
         break;
 
-      case "btn-result":
+      case 'btn-result':
         viewResult();
         break;
     }
   }
 });
 
-calculator.input.addEventListener("input", () => {
+calculator.input.addEventListener('input', () => {
   calculator.hideError();
 });
 
-calculator.input.addEventListener("keydown", (event) => {
-  if (event.code === "Backspace") {
-    calculator.delete();
-    // предотвращает второй вызов delete(),
-    // который происходит при всплытии
-    event.preventDefault();
-  } else if (event.code === "Enter") {
+calculator.input.addEventListener('keydown', (event) => {
+  if (
+    event.code === 'Backspace' ||
+    event.code === 'ArrowLeft' ||
+    event.code === 'ArrowRight'
+  ) {
+    return;
+  } else if (event.code === 'Enter') {
     viewResult();
   } else if (!checkCorrectCode(event.key)) {
     event.preventDefault();
@@ -81,11 +159,11 @@ function viewResult() {
   const result = calculator.getResult();
 
   if (result) {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerHTML = result;
 
     calculator.input.value = result;
-    document.querySelector(".history__list").append(li);
+    document.querySelector('.history__list').append(li);
   } else {
     calculator.showError();
   }
@@ -93,14 +171,14 @@ function viewResult() {
 
 function checkCorrectCode(key) {
   return (
-    (key >= "0" && key <= "9") ||
-    key === "+" ||
-    key === "-" ||
-    key === "*" ||
-    key === "/" ||
-    key === "%" ||
-    key === "(" ||
-    key === ")" ||
-    key === "."
+    (key >= '0' && key <= '9') ||
+    key === '+' ||
+    key === '-' ||
+    key === '*' ||
+    key === '/' ||
+    key === '%' ||
+    key === '(' ||
+    key === ')' ||
+    key === '.'
   );
 }
